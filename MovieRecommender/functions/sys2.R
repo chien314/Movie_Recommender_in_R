f369700c5671003c98695cb0c7320e3e94eccfa0
ken <kenwei93@gmail.com>
2020-12-14 22:25:59 -0500

    push in updates to avoid overrides for original source

--- MovieRecommender/functions/sys2.R
+++ MovieRecommender/functions/sys2.R
@@ -6,14 +6,7 @@
                      header = FALSE)
   colnames(ratings) = c('UserID', 'MovieID', 'Rating', 'Timestamp')
   
-  movies = readLines(paste0(myurl, 'movies.dat?raw=true'))
-  movies = strsplit(movies, split = "::", fixed = TRUE, useBytes = TRUE)
-  movies = matrix(unlist(movies), ncol = 3, byrow = TRUE)
-  movies = data.frame(movies, stringsAsFactors = FALSE)
-  colnames(movies) = c('MovieID', 'Title', 'Genres')
-  movies$MovieID = as.integer(movies$MovieID)
-  # convert accented characters
-  movies$Title = iconv(movies$Title, "latin1", "UTF-8")
+
   # extract year
   movies$Year = as.numeric(unlist(
     lapply(movies$Title, function(x) substr(x, nchar(x)-4, nchar(x)-1))))
